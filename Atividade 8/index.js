c6bd87e464ebd8fb9ca1ed44f17ddfa88dc0fd78
Breno009 <147253894+Breno009@users.noreply.github.com>
2023-11-19 11:44:51 -0300

    Add files via upload

---
+++ Atividade 8/index.js
@@ -0,0 +1,5 @@
+let number = 2023;
+let nome = prompt('Digite seu ano de nacismento: ')
+const oi = prompt('Digite seu nome: ')
+let idade = (number-nome)
+alert(`Olá, seu nome é ${oi} e você tem ${idade} anos`)
